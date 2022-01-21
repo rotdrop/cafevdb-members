3641978fb9df4cfbd2400922edb7d47cd667071a
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-21 12:58:34 +0100

    WIP

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 
 require_once __DIR__ . '/../../../tests/bootstrap.php';
+
+require_once __DIR__ . "/../vendor/autoload.php";
+$installedVersions = __DIR__ . "/../vendor/composer/InstalledVersions.php";
+if (file_exists($installedVersions)) {
+  require_once $installedVersions;
+}
