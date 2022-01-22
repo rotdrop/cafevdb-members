4e897d7a46dfe550cae1c13ef5fcc702150ee54c
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-23 00:33:36 +0100

    Proof-of-concept settings example using Vue.

--- src/PersonalSettings.vue
+++ src/PersonalSettings.vue
@@ -1,18 +1,45 @@
 <template>
   <SettingsSection :title="t('cafevdbmembers', 'CAFeVDB Database Connector')">
-    <SettingsInputText :id="'test-input'" :label="t('cafevdbmembers', 'Test Input')">
-    </SettingsInputText>
+    <SettingsInputText
+      :id="'test-input'"
+      v-model="inputTest"
+      :label="t('cafevdbmembers', 'Test Input')"
+      :hint="t('cafevdbmembers', 'Test Hint')"
+      @update="saveInputTest" />
   </SettingsSection>
 </template>
 
 <script>
 import SettingsSection from '@nextcloud/vue/dist/Components/SettingsSection'
-import SettingsInputText from '@nextcloud/vue/dist/Components/SettingsInputText'
+import SettingsInputText from './components/SettingsInputText'
+import { generateOcsUrl } from '@nextcloud/router'
+import axios from '@nextcloud/axios'
+
 export default {
   name: 'PersonalSettings',
   components: {
     SettingsSection,
     SettingsInputText,
+  },
+  data() {
+    return {
+      inputTest: '',
+    }
+  },
+  created() {
+    this.getData()
+  },
+  methods: {
+    async getData() {
+      const response = await axios.get(generateOcsUrl('apps/provisioning_api/api/v1/config/apps/cafevdbmembers/input_test'), {})
+      console.info('RESPONSE', response)
+      this.inputTest = response.data.ocs.data.data
+      console.info('VALUE', this.inputTest)
+    },
+    saveInputTest() {
+      console.info('SAVE INPUTTEST', this.inputTest)
+      OCP.AppConfig.setValue('cafevdbmembers', 'input_test', this.inputTest)
+    },
   },
 }
 </script>
