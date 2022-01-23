827ef39b05242a31b1f9bed75cbfe06978d2d583
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-24 00:34:27 +0100

    Add translation support.

---
+++ l10n/de_DE.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "cafevdbmembers",
+    {
+    "Camerata DB Members" : "Camerata DB Mitglieder",
+    "CAFeVDB Members" : "CAFeVDB Mitglieder",
+    "CAFeV DB Member's Connector" : "CAFeV DB Mitgliedsdata Integration",
+    "Expose a member's data record for the respective member" : "Stelle die Mitgliedsdaten dem jeweiligen Mitglied zur Verfügung"
+},
+"");
