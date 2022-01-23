3b5812f5d60e6a8346b9d81225acd5fdd49a79ee
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-24 00:55:54 +0100

    Update translations

--- l10n/de.js
+++ l10n/de.js
@@ -4,6 +4,10 @@ OC.L10N.register(
     "Camerata DB Members" : "Camerata DB Mitglieder",
     "CAFeVDB Members" : "CAFeVDB Mitglieder",
     "CAFeV DB Member's Connector" : "CAFeV DB Mitgliedsdata Integration",
-    "Expose a member's data record for the respective member" : "Stelle die Mitgliedsdaten dem jeweiligen Mitglied zur Verfügung"
+    "Expose a member's data record for the respective member" : "Stelle die Mitgliedsdaten dem jeweiligen Mitglied zur Verfügung",
+    "CAFeVDB Database Connector, Admin Settings" : "CAFeVDB Datenbank Integration, Administrator-Einstellungen",
+    "Test Input" : "Test-Eingabe",
+    "Test Hint" : "Test-Hinweis",
+    "CAFeVDB Database Connector, Personal Settings" : "CAFeVDB Datenbank Integration, perönliche Einstellungen"
 },
 "");
