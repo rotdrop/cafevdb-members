fd61dc524319245a3c2c84ea074ac274b50d209e
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-24 15:18:30 +0100

    Update translations.

--- l10n/de.js
+++ l10n/de.js
@@ -1,13 +1,23 @@
 OC.L10N.register(
     "cafevdbmembers",
     {
+    "Post to end-point \"%s\" not implemented." : "POST-Anfragen an \"%s\" sind nicht implementiert.",
+    "Post to base-url of app \"%s\" not allowed." : "POST-Anfragen an die Basis-Adresse der App \"%s\" sind nicht erlaubt.",
+    "Get from end-point \"%s\" not implemented." : "GET-Anfragen an \"%s\" sind nicht implementiert.",
+    "Sorry, not yet implemented!" : "'Tschuldigung, noch nicht implementiert!",
     "Camerata DB Members" : "Camerata DB Mitglieder",
+    "Error, caught an exception." : "Fehler, \"Ausnahme\" \"aufgefangen\".",
+    "Caused by previous exception" : "Verursacht durch eine vorhergehende Ausnahme",
     "CAFeVDB Members" : "CAFeVDB Mitglieder",
     "CAFeV DB Member's Connector" : "CAFeV DB Mitgliedsdata Integration",
     "Expose a member's data record for the respective member" : "Stelle die Mitgliedsdaten dem jeweiligen Mitglied zur Verfügung",
     "CAFeVDB Database Connector, Admin Settings" : "CAFeVDB Datenbank Integration, Administrator-Einstellungen",
     "Test Input" : "Test-Eingabe",
     "Test Hint" : "Test-Hinweis",
+    "Dummy New Button" : "Platzhalter \"Neu Knopf\"",
+    "Dummy Text" : "Platzhalter Text",
+    "reason unknown" : "Grund unbekannt",
+    "Could not fetch musician(s): {message}" : "Konnte den oder die Musiker nicht abfragen: {message}",
     "CAFeVDB Database Connector, Personal Settings" : "CAFeVDB Datenbank Integration, perönliche Einstellungen"
 },
 "");
