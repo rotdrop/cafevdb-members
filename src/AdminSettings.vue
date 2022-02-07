b828d3b05f4048e3db2ed54e9f9b3c02c75d21fc
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-02-07 22:51:47 +0100

    Prettify admin settings.

--- src/AdminSettings.vue
+++ src/AdminSettings.vue
@@ -28,7 +28,24 @@
       :label="t(appName, 'Member-Data Root-Folder')"
       :hint="t(appName, 'Specify the root folder below which all member-data will be mounted.')"
       @update="saveTextInput(...arguments, 'memberRootFolder' /* @todo: how to pass v-model? */)" />
-    <button class="button primary"
+    <div v-if="showSyncProgress">
+      <div class="sync-status">
+        <span class="sync-text">{{ syncText }}</span>
+        <button v-if="syncFinished"
+                class="button primary sync-clear"
+                :title="t(appName, 'Remove the status feedback from the last sync.')"
+                @click="hideProgressIfReady()">
+          {{ t(appName, 'Ok') }}
+        </button>
+        <span class="flex-spacer" />
+        <span class="sync-counter">{{ syncCounter }}</span>
+      </div>
+      <ProgressBar :value="syncPercentage"
+                   :error="syncError"
+                   size="medium" />
+    </div>
+    <button v-else
+            class="button primary"
             :title="t(appName, 'Synchronize the hierarchy of shared folders below {root} with the projects of the {managementApp}-orchestra-management app.', { root: memberRootFolder + '/', managementApp: 'cafevdb' })"
             @click="synchronizeFolders()">
       {{ t(appName, 'Synchronize Folder-Structure') }}
@@ -38,6 +55,7 @@
 
 <script>
 import { appName } from './config.js'
+import ProgressBar from '@nextcloud/vue/dist/Components/ProgressBar'
 import SettingsSection from '@nextcloud/vue/dist/Components/SettingsSection'
 import SettingsInputText from './components/SettingsInputText'
 import { generateUrl } from '@nextcloud/router'
@@ -49,21 +67,47 @@ export default {
   components: {
     SettingsSection,
     SettingsInputText,
+    ProgressBar
   },
   data() {
     return {
       memberRootFolder: '',
+      memberFolderGroups: [],
+      syncFailure: false,
+      syncTotals: 0,
+      syncDone: 0,
+      synchronizing: false,
+      syncLabel: '',
     }
   },
   created() {
     this.getData()
   },
+  computed: {
+    showSyncProgress() {
+      return this.synchronizing
+    },
+    syncPercentage() {
+      return this.syncTotals > 0 ? this.syncDone * 100 / this.syncTotals : 0
+    },
+    syncError() {
+      return this.syncFailure
+    },
+    syncText() {
+      return this.syncLabel
+    },
+    syncFinished() {
+      return (this.syncDone > 0 && this.syncDone == this.syncTotals) || this.syncFailure
+    }
+  },
   methods: {
     async getData() {
-      const response = await axios.get(generateUrl('apps/' + appName + '/settings/admin/memberRootFolder'), {})
-      console.info('RESPONSE', response)
+      let response = await axios.get(generateUrl('apps/' + appName + '/settings/admin/memberRootFolder'), {})
       this.memberRootFolder = response.data.value
-      console.info('VALUE', this.memberRootFolder)
+      console.info('ROOT FOLDER', this.memberRootFolder)
+      response = await axios.get(generateUrl('apps/' + appName + '/settings/admin/memberFolderGroups'), {})
+      this.memberFolderGroups = response.data.value
+      console.info('FOLDER GROUPS', this.memberFolderGroups)
     },
     async saveTextInput(value, settingsKey, force) {
       const self = this
@@ -102,16 +146,38 @@ export default {
     },
     async synchronizeFolders() {
       const self = this
-      try {
-        const response = await axios.post(generateUrl('apps/' + appName + '/settings/admin/synchronize'))
-        showSuccess(t(appName, 'Successfully synchronized the shared-folder structure with the orchestra projects'))
-      } catch (e) {
-        let message = t(appName, 'reason unknown')
-        if (e.response && e.response.data && e.response.data.message) {
-          message = e.response.data.message
-          console.info('RESPONSE', e.response)
+      this.synchronizing = true
+      this.syncTotals = this.memberFolderGroups.length
+      this.syncDone = 0
+      this.syncFailure = false
+      let group = null
+      for (group of this.memberFolderGroups) {
+        console.info('GROUP', group)
+        this.syncLabel = t(appName, 'Synchronizing for group {group}', { group: group.displayName })
+        this.syncCounter = t(appName, '{current} of {totals}', { current: this.syncDone + 1, totals: this.syncTotals })
+        try {
+          const response = await axios.post(generateUrl('apps/' + appName + '/settings/admin/synchronize', { value: group.gid }))
+        } catch (e) {
+          let message = t(appName, 'reason unknown')
+          if (e.response && e.response.data && e.response.data.message) {
+            message = e.response.data.message
+            console.info('RESPONSE', e.response)
+          }
+          showError(t(appName, 'Folder for {group} could not be created: {message}', { group: group.displayName, message }), { timeout: TOAST_PERMANENT_TIMEOUT })
+          this.syncFailure = true
+          break;
         }
-        showError(t(appName, 'Folder-structure could not be synchronized: {message}', { message }), { timeout: TOAST_PERMANENT_TIMEOUT })
+        ++this.syncDone
+      }
+      this.syncLabel = this.syncFailure
+        ? t(appName, 'Failed at group {group} after {numFolders} have been processed successfully, {remainingFolders} are remaining.',
+            { group: group.displayName, numFolders: this.syncDone, remainingFolders: this.syncTotals - this.syncDone } )
+        : t(appName, 'All done, folder structure for all {numFolders} folders is up to date.', { numFolders: this.syncTotals })
+    },
+    hideProgressIfReady() {
+      if (this.syncDone >= this.syncTotals) {
+        this.synchronizing = false
+        this.syncFailure = false
       }
     },
   },
@@ -127,6 +193,19 @@ export default {
       background-size:45px;
       background-position:left center;
       height:30px;
+    }
+    .sync-status {
+      display:flex;
+      flex-direction:row;
+      align-items:center;
+      width:100%;
+      .flex-spacer {
+        flex-grow:4;
+        height:34px
+      }
+      button.sync-clear {
+        margin-left:1ex;
+      }
     }
   }
 </style>
