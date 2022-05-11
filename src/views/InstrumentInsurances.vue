547b52d9ebd42e4e2579715a897c4bca304c1de8
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-05-11 14:27:48 +0200

    Initial setup for instrument insurances.

--- src/views/InstrumentInsurances.vue
+++ src/views/InstrumentInsurances.vue
@@ -23,29 +23,123 @@
 </script>
 <template>
   <Content :class="'app-' + appName" :app-name="'app-' + appName">
-    <div>
-      <h2>{{ t(appName, 'To be implemented: {view}', { view: t(appName, 'Instrument Insurances'), }) }}</h2>
-      <div>{{ t(appName, 'This is currently only a placeholder for the future plan to make the personal data of the orchestra members available to just the respective orchestra member.') }}</div>
+    <div v-if="loading" class="page-container loading" />
+    <div v-else class="page-container">
+      <h2>{{ t(appName, 'Instrument Insurances of {publicName}', {publicName: memberData.personalPublicName }) }}</h2>
+      <div class="debug-container">
+        <CheckboxRadioSwitch :checked.sync="debug">
+          {{ t(appName, 'Enable Debug') }}
+        </CheckboxRadioSwitch>
+        <div v-if="debug" class="debug">
+          <div>{{ t(appName, 'DEBUG: all data') }}</div>
+          <pre>{{ JSON.stringify(memberData, null, 2) }}</pre>
+        </div>
+      </div>
     </div>
   </Content>
 </template>
 <script>
+
+import { appName } from '../config.js'
+import Vue from 'vue'
 import Content from '@nextcloud/vue/dist/Components/Content'
+import ListItem from '@nextcloud/vue/dist/Components/ListItem'
+import CheckboxRadioSwitch from '@nextcloud/vue/dist/Components/CheckboxRadioSwitch'
+import '@nextcloud/dialogs/styles/toast.scss'
+import { generateUrl } from '@nextcloud/router'
+import { showError, TOAST_PERMANENT_TIMEOUT } from '@nextcloud/dialogs'
+import axios from '@nextcloud/axios'
+import moment from '@nextcloud/moment'
+
 export default {
   name: 'InstrumentInsurances',
   components: {
     Content,
+    CheckboxRadioSwitch,
+    ListItem,
   },
+  mixins: [
+    {
+      methods: {
+        moment,
+      },
+    },
+  ],
   data() {
     return {
+      memberData: {},
       loading: true,
+      debug: false,
     }
   },
-  async mounted() {
-    console.info('MOUNTED')
-    this.loading = false
+  async created() {
+    const self = this;
+    try {
+      const response = await axios.get(generateUrl('/apps/' + appName + '/member'))
+      for (const [key, value] of Object.entries(response.data)) {
+        Vue.set(this.memberData, key, value)
+      }
+    } catch (e) {
+      console.error('ERROR', e)
+      let message = t(appName, 'reason unknown')
+      if (e.response && e.response.data && e.response.data.message) {
+        message = e.response.data.message
+        console.info('RESPONSE', e.response)
+      }
+      // Ignore for the time being
+      if (this === false) {
+        showError(t(appName, 'Could not fetch musician(s): {message}', { message }), { timeout: TOAST_PERMANENT_TIMEOUT })
+      }
+    }    this.loading = false
   },
   methods: {
+    formatDate(date, flavour) {
+      flavour = flavour || 'medium'
+      switch (flavour) {
+        case 'short':
+        case 'medium':
+        case 'long':
+          return moment(date).format('L');
+      }
+      return moment(data).format(flavour);
+    },
   },
 }
 </script>
+<style lang="scss" scoped>
+.page-container {
+  padding-left:0.5rem;
+  &.loading {
+    width:100%;
+  }
+}
+
+.debug-container {
+  width:100%;
+}
+
+::v-deep {
+  .list-item {
+    padding-right: 0;
+    ul .list-item {
+      padding-top:2px;
+      padding-bottom:2px;
+    }
+  }
+
+  .line-two__subtitle {
+    padding-right:0;
+  }
+
+  .line-one--bold {
+    &.line-one {
+      .line-one__details {
+        font-weight:inherit;
+      }
+    }
+    &.line-two {
+      font-weight: normal;
+    }
+  }
+}
+</style>
