e1fbc859c2db41ed562a58e58feca307e64df0f5
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-12-18 20:33:41 +0100

    Shutup anoying styleint w.r.t. v-deep.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,13 @@
-const nextcloudStylelintConfig = require('@nextcloud/stylelint-config')
-
 module.exports = {
   extends: [
     '@nextcloud/stylelint-config',
   ],
   rules: {
     indentation: 2,
+    'selector-pseudo-element-no-unknown': [
+      true, {
+        ignorePseudoElements: ['v-deep'],
+      },
+    ],
   },
 }
