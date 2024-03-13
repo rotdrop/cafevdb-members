5ebc8e385ef4f890f061ba670956e4486529ac9d
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-03-13 13:35:20 +0100

    git subrepo pull --force php-toolkit
    subrepo:
      subdir:   "php-toolkit"
      merged:   "af05f85"
    upstream:
      origin:   "git@github.com:rotdrop/nextcloud-app-toolkit.git"
      branch:   "main"
      commit:   "af05f85"
    git-subrepo:
      version:  "0.4.5"
      origin:   "???"
      commit:   "???"

--- php-toolkit/Traits/UtilTrait.php
+++ php-toolkit/Traits/UtilTrait.php
@@ -3,7 +3,7 @@
  * A collection of reusable traits classes for Nextcloud apps.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright 2022-2024 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2022, 2023 Claus-Justus Heine <himself@claus-justus-heine.de>
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
