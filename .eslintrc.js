64226605d561d7ae0717c3641ad81d68c9ceb959
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-02-01 12:01:03 +0100

    Convert .eslintrc to a form which is easier to merge between branches.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,32 +6,80 @@ module.exports = {
     'import/resolver': {
       typescript: {},
       node: {
-        extensions: ['.js', '.jsx', '.d.ts', '.ts', '.tsx', '.vue'],
+        extensions: [
+          '.d.ts',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.vue',
+        ],
       },
     },
   },
   rules: {
-    'no-tabs': ['error', { allowIndentationTabs: false }],
-    indent: ['error', 2],
+    'no-tabs': [
+      'error',
+      {
+        allowIndentationTabs: false,
+      },
+    ],
+    indent: [
+      'error',
+      2,
+    ],
     'no-mixed-spaces-and-tabs': 'error',
-    'vue/html-indent': ['error', 2],
+    'vue/html-indent': [
+      'error',
+      2,
+    ],
     // Do allow line-break before closing brackets
-    'vue/html-closing-bracket-newline': ['error', { singleline: 'never', multiline: 'always' }],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        multiline: 'always',
+        singleline: 'never',
+      },
+    ],
     // space before self-closing elements
     'vue/html-closing-bracket-spacing': 'error',
   },
   overrides: [
     {
-      files: ['src/toolkit/**', 'webpack.config.js'],
+      files: [
+        'src/toolkit/**',
+        'webpack.config.js',
+      ],
       rules: {
-        semi: ['error', 'always'],
+        semi: [
+          'error',
+          'always',
+        ],
       },
     },
     {
-      files: ['*.ts', '*.cts', '*.mts', '*.tsx', '*.vue'],
+      files: [
+        '*.cts',
+        '*.mts',
+        '*.ts',
+        '*.tsx',
+        '*.vue',
+      ],
       rules: {
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-        'import/no-unresolved': ['error', { ignore: ['cafevdbmembers'] }],
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+          },
+        ],
+        'import/no-unresolved': [
+          'error',
+          {
+            ignore: [
+              'cafevdbmembers',
+            ],
+          },
+        ],
       },
     },
   ],
