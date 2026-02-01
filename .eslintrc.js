329e9ee13ac75e446ecfa696924995d506754b3a
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-02-01 12:04:59 +0100

    Format .eslintrc.js s.t. it is easier to merge branches

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,71 @@
 module.exports = {
-  // extends: [
-  //   '@nextcloud',
-  // ],
+  extends: [
+    '@nextcloud',
+  ],
   rules: {
-    'no-tabs': ['error', { allowIndentationTabs: false }],
-    indent: ['error', 2],
+    'no-tabs': [
+      'error',
+      {
+        allowIndentationTabs: false,
+      },
+    ],
+    indent: [
+      'error',
+      2,
+    ],
     'no-mixed-spaces-and-tabs': 'error',
-    'vue/html-indent': ['error', 2],
+    'vue/html-indent': [
+      'error',
+      2,
+    ],
     // Do allow line-break before closing brackets
-    'vue/html-closing-bracket-newline': ['error', { singleline: 'never', multiline: 'always' }],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        multiline: 'always',
+        singleline: 'never',
+      },
+    ],
     // space before self-closing elements
     'vue/html-closing-bracket-spacing': 'error',
   },
   overrides: [
     {
-      files: ['src/toolkit/**', 'build/ts-types/**'],
+      files: [
+        'build/ts-types/**',
+        'src/toolkit/**',
+      ],
       rules: {
-        semi: ['error', 'always'],
+        semi: [
+          'error',
+          'always',
+        ],
       },
     },
     {
-      files: ['*.ts', '*.cts', '*.mts', '*.tsx', '*.vue'],
+      files: [
+        '*.cts',
+        '*.mts',
+        '*.ts',
+        '*.tsx',
+        '*.vue',
+      ],
       rules: {
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+          },
+        ],
         // Note: you must disable the base rule as it can report incorrect errors
         'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error', { functions: false, variables: false }],
+        '@typescript-eslint/no-use-before-define': [
+          'error',
+          {
+            functions: false,
+            variables: false,
+          },
+        ],
       },
     },
   ],
