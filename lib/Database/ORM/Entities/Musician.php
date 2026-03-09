191382312898e76463bc882185b726ba7e9ae3ea
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-03-09 10:56:34 +0100

    Entities\Musician, fix wrong join table name.

--- lib/Database/ORM/Entities/Musician.php
+++ lib/Database/ORM/Entities/Musician.php
@@ -3,7 +3,7 @@
  * Member's data base connector for CAFEVDB orchetra management app.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
- * @copyright Copyright (c) 2022, 2023, 2025 Claus-Justus Heine
+ * @copyright Copyright (c) 2022, 2023, 2025, 2026 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
@@ -242,7 +242,7 @@ class Musician implements \ArrayAccess, \JsonSerializable
   /**
    * @var Collection
    */
-  #[ORM\JoinTable(name: 'EncryptedFileOwners')]
+  #[ORM\JoinTable(name: 'PersonalizedEncryptedFileOwnersView')]
   #[ORM\ManyToMany(targetEntity: EncryptedFile::class, inversedBy: 'owners', indexBy: 'id', fetch: 'EXTRA_LAZY')]
   private $encryptedFiles;
 
